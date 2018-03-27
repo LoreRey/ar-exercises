2f1762a7d0eae82158f3cf614f9bbaaa13b5549b
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-03-27 20:43:08 +0000

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name:"
+@store_name = gets.chomp
+@store_name = Store.create(name: @store_name)
+
+@store_name.errors.messages.each do |err|
+  puts err
+end
+
